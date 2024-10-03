0c489f0006a3e0ffdd11c156867045128831ab89
Ritika-Goyal11 <156280236+Ritika-Goyal11@users.noreply.github.com>
2024-10-04 00:47:08 +0545

    Update app1.py

--- app1.py
+++ app1.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello World! Ritika here, how are you?'
+    return 'Hello World! Ritika here'
 
 # main driver function
 if __name__ == '__main__':
